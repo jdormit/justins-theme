e9bf9335e8b76cbf07fe684d0336e07d9a12e382
Jeremy Dormitzer <jdormitzer@hubspot.com>
2018-01-29 16:36:25 -0700

    Format

--- single-portfolio.php
+++ single-portfolio.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()): ?>
-<?php while (have_posts()): the_post(); ?>
+    <?php while (have_posts()): the_post(); ?>
 
-<h1 class="portfolioTitle"><?php the_title(); ?></h1>
-<div class="portfolioContent">
-    <?php the_content(); ?>
-</div>
+        <h1 class="portfolioTitle"><?php the_title(); ?></h1>
+        <div class="portfolioContent">
+            <?php the_content(); ?>
+        </div>
 
-<?php endwhile; ?>
+    <?php endwhile; ?>
 <?php endif; ?>
 
 <?php get_footer(); ?>
